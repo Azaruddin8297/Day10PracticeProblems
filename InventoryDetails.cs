d516efacdca252436e4801f558feb0d045ec31c7
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-24 15:49:40 +0530

    InventoryManger

---
+++ InventoryDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JasonFile
+{
+    public class InventoryDetails
+    {
+        public List<TypesofRice> typesofRice;
+        public List<TypesofWheat> typesofWheat;
+        public List<TypesofPulses> typesofpulses;
+    }
+    public class TypesofRice
+    {
+        public string Name;
+        public int Weight;
+        public int Price;
+    }
+    public class TypesofWheat
+    {
+        public string Name;
+        public int Weight;
+        public int Price;
+    }
+    public class TypesofPulses
+    {
+        public string Name;
+        public int Weight;
+        public int Price;
+    }
+}
---
+++ ReadInventory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JasonFile
+{
+    public class ReadInventory
+    {
+        public InventoryDetails Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+                }
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+    }
+    } }
